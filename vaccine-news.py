b2142e7526821a32cfff6aae84dff6eab72d76e0
douglas0098 <douglas0098henrique2@gmail.com>
2024-04-29 14:22:06 -0300

    Refatoramento do projeto

--- vaccine-news.py
+++ vaccine-news.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, flash, redirect, render_template, request, session, url_for, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
+import os
 
+
+path = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__)
-app.config('config.py')
 
-db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
-    return render_template()
+    return render_template('index.html')
+
+@app.route('/noticias')
+def noticias():
+    return render_template('noticias.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
