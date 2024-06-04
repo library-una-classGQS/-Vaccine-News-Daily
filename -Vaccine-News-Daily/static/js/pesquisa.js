d09f9e624e973bb61e09b94cf6b7aa1249d69777
Wilker Gandra <wilkerbruno@users.noreply.github.com>
2024-06-04 10:55:54 -0300

    pesquisa
    corrigindo a pagina de pesquisa e as deixando alinhadas

--- -Vaccine-News-Daily/static/js/pesquisa.js
+++ -Vaccine-News-Daily/static/js/pesquisa.js
@@ -1,5 +1,5 @@
 
-const searchForm = document.getElementById('search-form');
+/* const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
 const searchResults = document.getElementById('search-results');
@@ -30,6 +30,47 @@ searchButton.addEventListener('click', (e) => {
       searchResults.style.display = 'block';
     })
    .catch(error => console.error(error));
+  }
+});
+
+searchInput.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    searchButton.click();
+  }
+});
+ */
+
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+const searchResults = document.getElementById('search-results');
+
+searchButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  const searchTerm = searchInput.value.trim();
+  if (searchTerm) {
+    fetch('/search', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ searchTerm })
+    })
+    .then(response => response.json())
+    .then(data => {
+      const results = data.results;
+      searchResults.innerHTML = '';
+      results.forEach((result) => {
+        const resultHTML = `
+          <li>
+            <a href="${result.url}">${result.title}</a>
+          </li>
+        `;
+        searchResults.innerHTML += resultHTML;
+      });
+      searchResults.style.display = 'block';
+    })
+    .catch(error => console.error(error));
   }
 });
 
